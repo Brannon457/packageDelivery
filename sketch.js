8a070ac27067e9b0f83afd9a4a5b3dccc8265d39
Brannon457 <76511279+Brannon457@users.noreply.github.com>
2021-01-24 14:44:26 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,112 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine, world;
-var box1, pig1;
-var backgroundImage;
-function preload(){
-    backgroundImage = loadImage("sprites/bg.png");
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-    
-    ground = new Ground(600,height,1200,20)
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+	
+	
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+	engine = Engine.create();
+	world = engine.world;
 
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+  
 }
 
-function draw(){
-    background(backgroundImage);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  //packageBody.x=World. helicopterSprite;
+  //packageBody.y=World. helicopterSprite;
 
-    box5.display();
-    log4.display();
-    log5.display();
+  if(keyDown("RIGHT")){
+	helicopterSprite.velocityX = 5;
+}
+if(keyDown("LEFT")){
+	helicopterSprite.velocityX = -5;
+}
 
-    bird.display();
+if(keyDown("SPACE")){
+	helicopterSprite.velocityX = 0;
+}
+
+if(keyDown("RIGHT")){
+	packageBody.velocityX = 5;
+}
+if(keyDown("LEFT")){
+	packageBody.velocityX = -5;
+}
+
+if(keyDown("SPACE")){
+	packageBody.velocityX = 0;
+}
+
+if(keyDown("DOWN")){
+	Matter.Body.setStatic(packageBody, false);
+}
+  packageSprite.x = packageBody.position.x 
+  packageSprite.y = packageBody.position.y 
+
+
+  drawSprites();
+  
+  
+ 
 }
